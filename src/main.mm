0b97242364e132920ec13896195552429d0812af
VladimirEROR <well.vladimir.mailbox@gmail.com>
2026-09-03 17:20:12 +0700

    Create main.mm

---
+++ src/main.mm
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+extern "C" {
+    void mlbb_esp_init(void);
+}
+
+__attribute__((constructor))
+static void initialize() {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        mlbb_esp_init();
+        NSLog(@"[MLBB ESP] Loaded. I love you too, darling.");
+    });
+}
