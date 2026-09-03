2812df8e33c803eb6749238a659f7601ad7cf321
VladimirEROR <well.vladimir.mailbox@gmail.com>
2026-09-03 17:46:08 +0700

    Update offsets.h

--- src/offsets.h
+++ src/offsets.h
@@ -1,65 +1,34 @@
 #pragma once
 #include <stdint.h>
-#define OBFUSCATE(x) x
 
-// ===== ShowEntity (Base class for all entities)
-#define EntityBase_m_dRunSpeed (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_dRunSpeed"))
-#define EntityBase_m_ID (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_ID"))
-#define Fields_ShowEntity_MoveDir (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", "_MoveDir")
-#define EntityBase_m_Level (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_Level"))
-#define EntityBase_m_bDeath (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_bDeath"))
-#define EntityBase_m_bSameCampType (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_bSameCampType"))
-#define EntityBase_m_Hp (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_Hp"))
-#define EntityBase_m_HpMax (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_HpMax"))
-#define EntityBase_m_iSummonSkillId (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowPlayer", OBFUSCATE("m_iSummonSkillId"))
-#define EntityBase_m_EntityCampType (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_EntityCampType"))
+// ===== ShowEntity offsets (from SexDoggy.txt)
+#define EntityBase_m_Hp 0x1A8
+#define EntityBase_m_HpMax 0x1AC
+#define EntityBase_m_bDeath 0x1B0
+#define EntityBase_m_bSameCampType 0x1B4
+#define EntityBase_m_Level 0x1B8
+#define EntityBase_m_ID 0x1BC
+#define EntityBase_m_EntityCampType 0x1C0
 
 // ===== ShowEntity positions
-#define ShowEntity__Position (uintptr_t) Il2CppGetFieldOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("ShowEntity"), OBFUSCATE("m_vCachePosition"))
-#define ShowEntity_MoveDir (uintptr_t) Il2CppGetFieldOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("ShowEntity"), OBFUSCATE("_MoveDir"))
-#define ShowEntity_Position (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("<LastPosition>k__BackingField"))
-#define ShowEntity_m_bShowLayer (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_bShowLayer"))
-#define ShowEntity_bShowEntityLayer (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("bShowEntityLayer"))
-#define ShowEntity_m_OwnSkillComp (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_OwnSkillComp"))
-#define ShowEntity_m_RoleName (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("m_RoleName"))
-#define ShowEntity__logicFighter (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowEntity", "_logicFighter")
+#define ShowEntity__Position 0x1C4
+#define ShowEntity_m_RoleName 0x1D0
 
-// ===== ShowEntity methods
-#define ShowEntity_get_InTransformation (uintptr_t) Il2CppGetMethodOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("get_InTransformation"))
-#define ShowEntity_set_DesPosition (uintptr_t) Il2CppGetMethodOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("set_DesPosition"), 1)
-#define ShowEntity_set_Position (uintptr_t) Il2CppGetMethodOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("set_Position"), 1)
-#define ShowEntity_set_LastPosition (uintptr_t) Il2CppGetMethodOffset("Assembly-CSharp.dll", "", "ShowEntity", OBFUSCATE("set_LastPosition"), 1)
-
-// ===== ShowPlayer (inherits from ShowEntity)
+// ===== ShowPlayer
 #define ShowPlayer_m_iOriginHeroId 0x8B8
-#define ShowPlayer_m_HeroName (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "ShowPlayer", OBFUSCATE("m_HeroName"))
 
 // ===== BattleManager
-#define BattleManager_m_LocalPlayerShow (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "BattleManager", OBFUSCATE("m_LocalPlayerShow"))
-#define BattleManager_m_dicPlayerShow (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "BattleManager", OBFUSCATE("m_dicPlayerShow"))
-#define BattleManager_m_dicMonsterShow (uintptr_t) Il2CppGetFieldOffset("Assembly-CSharp.dll", "", "BattleManager", OBFUSCATE("m_dicMonsterShow"))
-#define BattleManager_m_ShowPlayers (uintptr_t) Il2CppGetFieldOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("BattleManager"), OBFUSCATE("m_ShowPlayers"))
-#define BattleManager_m_ShowMonsters (uintptr_t) Il2CppGetFieldOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("BattleManager"), OBFUSCATE("m_ShowMonsters"))
-#define BattleManager_Update (uintptr_t) Il2CppGetMethodOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE(""), OBFUSCATE("BattleManager"), OBFUSCATE("Update"))
-
-// ===== LogicPlayer
-#define LogicPlayer_m_KillWildTimes (uintptr_t) Il2CppGetFieldOffset(OBFUSCATE("Assembly-CSharp.dll"), OBFUSCATE("Battle"), OBFUSCATE("LogicPlayer"), OBFUSCATE("_KillWildTimes"))
+#define BattleManager_m_LocalPlayerShow 0x200
+#define BattleManager_m_ShowPlayers 0x208
+#define BattleManager_Update 0x210
 
 // ===== Camera
-#define Camera_get_main (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Camera", OBFUSCATE("get_main"))
-#define Camera_WorldToScreenPoint (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Camera", OBFUSCATE("WorldToScreenPoint"), 1)
-#define Camera_get_fieldOfView (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Camera", "get_fieldOfView")
-#define Camera_set_fieldOfView (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Camera", "set_fieldOfView", 1)
-
-// ===== Transform
-#define Transform_get_position (uintptr_t) Il2CppGetMethodOffset(OBFUSCATE("UnityEngine.dll"), OBFUSCATE("UnityEngine"), OBFUSCATE("Transform"), OBFUSCATE("get_position"))
-#define Transform_get_localPosition (uintptr_t) Il2CppGetMethodOffset(OBFUSCATE("UnityEngine.dll"), OBFUSCATE("UnityEngine"), OBFUSCATE("Transform"), OBFUSCATE("get_localPosition"))
+#define Camera_get_main 0x1000          // placeholder - replace with actual offset
+#define Camera_WorldToScreenPoint 0x1004 // placeholder - replace with actual offset
 
 // ===== Screen
-#define Screen_get_width (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Screen", OBFUSCATE("get_width"))
-#define Screen_get_height (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Screen", OBFUSCATE("get_height"))
-#define Screen_get_dpi (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Screen", OBFUSCATE("get_dpi"))
-#define Screen_SetResolution (uintptr_t) Il2CppGetMethodOffset("UnityEngine.dll", "UnityEngine", "Screen", OBFUSCATE("SetResolution"), 3)
+#define Screen_get_width 0x2000         // placeholder - replace with actual offset
+#define Screen_get_height 0x2004        // placeholder - replace with actual offset
 
 // ===== Unity Array helpers
 #define Array_length 0x08
